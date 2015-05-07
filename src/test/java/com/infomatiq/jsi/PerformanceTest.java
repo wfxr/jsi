47f31cac38a1685ee7a84372313398b84921a1a1
Sebastian Kürten <sebastian.kuerten@fu-berlin.de>
2015-05-07 16:51:14 +0200

    Changed to gradle as buildsystem
    Removed maven specific stuff
    
    Removed SIL references because they can't be resolved using maven
    central
    
    Gradle artifact has '-topobyte' suffix and is pushed to a private
    repository, change at your convenience

--- src/test/java/com/infomatiq/jsi/PerformanceTest.java
+++ src/test/java/com/infomatiq/jsi/PerformanceTest.java
@@ -27,119 +27,103 @@ import org.slf4j.LoggerFactory;
 
 /**
  * PerformanceTest
- *  
- * Generates results used for comparing the performance of the Java Spatial 
+ * 
+ * Generates results used for comparing the performance of the Java Spatial
  * Index library against alternative implementations.
  */
-public class PerformanceTest extends TestCase {
+public class PerformanceTest extends TestCase
+{
 
-  private static final Logger log = LoggerFactory.getLogger(PerformanceTest.class);
-  private Script script = new Script();
-  
-  public PerformanceTest(String s) {
-    super(s);
-  }
-  
-  // Tests add, intersect, nearest, nearestn, contains.
-  // Can optimize for add performance, memory efficiency, or query performance.
-  //
-  public void testQueryPerformance() {
-    
-    // Test 1: add performance. 
-    // To acheive maximum add performance, it is necessary to minimize the number of
-    // node splits. Therefore set the MinNodeEntries to 1.
-    // do each test 3 times to see if there is any variance due to hotspot VM (or something else).
-    Properties p = new Properties();
-    
-    p.setProperty("MinNodeEntries", "1");
-    p.setProperty("MaxNodeEntries", "10");
-    p.setProperty("TreeVariant", "Linear");
-  
-    script.run("rtree.RTree", p, "allqueries-10000", Script.PERFORMANCE);
-    script.run("rtree.RTree", p, "allqueries-10000", Script.PERFORMANCE);
-    script.run("rtree.RTree", p, "allqueries-10000", Script.PERFORMANCE);
-    
-//    script.run("test.RTreeWrapper", p, "allqueries-10000", Script.PERFORMANCE);  
-//    script.run("test.RTreeWrapper", p, "allqueries-10000", Script.PERFORMANCE);  
-//    script.run("test.RTreeWrapper", p, "allqueries-10000", Script.PERFORMANCE);  
-//    
-//    script.run("SILWrapper",   p, "allqueries-10000", Script.PERFORMANCE);
-//    script.run("SILWrapper",   p, "allqueries-10000", Script.PERFORMANCE);
-//    script.run("SILWrapper",   p, "allqueries-10000", Script.PERFORMANCE);
-//    
-//    p.setProperty("TreeVariant", "Quadratic");
-//    script.run("SILWrapper",   p, "allqueries-10000", Script.PERFORMANCE);
-//    script.run("SILWrapper",   p, "allqueries-10000", Script.PERFORMANCE);
-//    script.run("SILWrapper",   p, "allqueries-10000", Script.PERFORMANCE);
-//    
-//    p.setProperty("TreeVariant", "Rstar");
-//    script.run("SILWrapper",   p, "allqueries-10000", Script.PERFORMANCE);
-//    script.run("SILWrapper",   p, "allqueries-10000", Script.PERFORMANCE);
-//    script.run("SILWrapper",   p, "allqueries-10000", Script.PERFORMANCE);
-  }
+	private static final Logger log = LoggerFactory
+			.getLogger(PerformanceTest.class);
+	private Script script = new Script();
 
-  public void testNearestN() {
-    Properties p = new Properties();
-    p.setProperty("MinNodeEntries", "5");
-    p.setProperty("MaxNodeEntries", "10");
-     
-    script.run("rtree.RTree",       p, "nearestN-100", Script.PERFORMANCE);
-    script.run("rtree.RTree",       p, "nearestN-1000", Script.PERFORMANCE);
-    script.run("rtree.RTree",       p, "nearestN-10000", Script.PERFORMANCE);    
-  }
-  
-  /**
-   * Tests performance of all the RTree variants for add() and intersect(),
-   * for up to 10,000,000 entries
-   */
-  public void testAllFunctions() {
-    log.debug("testAllFunctions()");
-    
-    Properties p = new Properties();
-       
-    // SimpleIndex and NullIndex do not use Min/MaxNodeEntries, so do them first.
-    script.run("SimpleIndex", p, "allfunctions-100", Script.PERFORMANCE);
-    script.run("SimpleIndex", p, "allfunctions-1000", Script.PERFORMANCE);
-    script.run("SimpleIndex", p, "allfunctions-10000", Script.PERFORMANCE);
-    // Only go up to 10000 for simple index, as it takes too int
-    
-    p.setProperty("TreeVariant", "null");
-    script.run("NullIndex",   p, "allfunctions-100", Script.PERFORMANCE);
-    script.run("NullIndex",   p, "allfunctions-1000", Script.PERFORMANCE);
-    script.run("NullIndex",   p, "allfunctions-10000", Script.PERFORMANCE);
-    //script.run("NullIndex",   p, "allfunctions-100000", Script.PERFORMANCE);
-    
-    p.setProperty("MinNodeEntries", "5");
-    p.setProperty("MaxNodeEntries", "20");  // reasonable values?
-    
-    p.setProperty("TreeVariant", "Linear");
-    script.run("RTreeWrapper", p, "allfunctions-100", Script.PERFORMANCE);
-    script.run("RTreeWrapper", p, "allfunctions-1000", Script.PERFORMANCE);
-    script.run("RTreeWrapper", p, "allfunctions-10000", Script.PERFORMANCE);
-    //script.run("RTreeWrapper", p, "allfunctions-100000", Script.PERFORMANCE);
-      
-    p.setProperty("TreeVariant", "Linear");
-    script.run("rtree.RTree",       p, "allfunctions-100", Script.PERFORMANCE);
-    script.run("rtree.RTree",       p, "allfunctions-1000", Script.PERFORMANCE);
-    script.run("rtree.RTree",       p, "allfunctions-10000", Script.PERFORMANCE);
-    //script.run("rtree.RTree",       p, "allfunctions-100000", Script.PERFORMANCE);
-      
-    p.setProperty("TreeVariant", "Linear");
-    script.run("SILWrapper",   p, "allfunctions-100", Script.PERFORMANCE);
-    script.run("SILWrapper",   p, "allfunctions-1000", Script.PERFORMANCE);
-    script.run("SILWrapper",   p, "allfunctions-10000", Script.PERFORMANCE);
-    //script.run("SILWrapper",   p, "allfunctions-100000", Script.PERFORMANCE);
-      
-    p.setProperty("TreeVariant", "Quadratic");
-    script.run("SILWrapper",   p, "allfunctions-100", Script.PERFORMANCE);
-    script.run("SILWrapper",   p, "allfunctions-1000", Script.PERFORMANCE);
-    script.run("SILWrapper",   p, "allfunctions-10000", Script.PERFORMANCE);
-    //script.run("SILWrapper",   p, "allfunctions-100000", Script.PERFORMANCE);
-      
-    p.setProperty("TreeVariant", "Rstar");
-    script.run("SILWrapper",   p, "allfunctions-100", Script.PERFORMANCE);
-    script.run("SILWrapper",   p, "allfunctions-1000", Script.PERFORMANCE);
-    script.run("SILWrapper",   p, "allfunctions-10000", Script.PERFORMANCE);
-    //script.run("SILWrapper",   p, "allfunctions-100000", Script.PERFORMANCE);
-  }
+	public PerformanceTest(String s)
+	{
+		super(s);
+	}
+
+	// Tests add, intersect, nearest, nearestn, contains.
+	// Can optimize for add performance, memory efficiency, or query
+	// performance.
+	//
+	public void testQueryPerformance()
+	{
+
+		// Test 1: add performance.
+		// To acheive maximum add performance, it is necessary to minimize the
+		// number of
+		// node splits. Therefore set the MinNodeEntries to 1.
+		// do each test 3 times to see if there is any variance due to hotspot
+		// VM (or something else).
+		Properties p = new Properties();
+
+		p.setProperty("MinNodeEntries", "1");
+		p.setProperty("MaxNodeEntries", "10");
+		p.setProperty("TreeVariant", "Linear");
+
+		script.run("rtree.RTree", p, "allqueries-10000", Script.PERFORMANCE);
+		script.run("rtree.RTree", p, "allqueries-10000", Script.PERFORMANCE);
+		script.run("rtree.RTree", p, "allqueries-10000", Script.PERFORMANCE);
+
+		// script.run("test.RTreeWrapper", p, "allqueries-10000",
+		// Script.PERFORMANCE);
+		// script.run("test.RTreeWrapper", p, "allqueries-10000",
+		// Script.PERFORMANCE);
+		// script.run("test.RTreeWrapper", p, "allqueries-10000",
+		// Script.PERFORMANCE);
+	}
+
+	public void testNearestN()
+	{
+		Properties p = new Properties();
+		p.setProperty("MinNodeEntries", "5");
+		p.setProperty("MaxNodeEntries", "10");
+
+		script.run("rtree.RTree", p, "nearestN-100", Script.PERFORMANCE);
+		script.run("rtree.RTree", p, "nearestN-1000", Script.PERFORMANCE);
+		script.run("rtree.RTree", p, "nearestN-10000", Script.PERFORMANCE);
+	}
+
+	/**
+	 * Tests performance of all the RTree variants for add() and intersect(),
+	 * for up to 10,000,000 entries
+	 */
+	public void testAllFunctions()
+	{
+		log.debug("testAllFunctions()");
+
+		Properties p = new Properties();
+
+		// SimpleIndex and NullIndex do not use Min/MaxNodeEntries, so do them
+		// first.
+		script.run("SimpleIndex", p, "allfunctions-100", Script.PERFORMANCE);
+		script.run("SimpleIndex", p, "allfunctions-1000", Script.PERFORMANCE);
+		script.run("SimpleIndex", p, "allfunctions-10000", Script.PERFORMANCE);
+		// Only go up to 10000 for simple index, as it takes too int
+
+		p.setProperty("TreeVariant", "null");
+		script.run("NullIndex", p, "allfunctions-100", Script.PERFORMANCE);
+		script.run("NullIndex", p, "allfunctions-1000", Script.PERFORMANCE);
+		script.run("NullIndex", p, "allfunctions-10000", Script.PERFORMANCE);
+		// script.run("NullIndex", p, "allfunctions-100000",
+		// Script.PERFORMANCE);
+
+		p.setProperty("MinNodeEntries", "5");
+		p.setProperty("MaxNodeEntries", "20"); // reasonable values?
+
+		p.setProperty("TreeVariant", "Linear");
+		script.run("RTreeWrapper", p, "allfunctions-100", Script.PERFORMANCE);
+		script.run("RTreeWrapper", p, "allfunctions-1000", Script.PERFORMANCE);
+		script.run("RTreeWrapper", p, "allfunctions-10000", Script.PERFORMANCE);
+		// script.run("RTreeWrapper", p, "allfunctions-100000",
+		// Script.PERFORMANCE);
+
+		p.setProperty("TreeVariant", "Linear");
+		script.run("rtree.RTree", p, "allfunctions-100", Script.PERFORMANCE);
+		script.run("rtree.RTree", p, "allfunctions-1000", Script.PERFORMANCE);
+		script.run("rtree.RTree", p, "allfunctions-10000", Script.PERFORMANCE);
+		// script.run("rtree.RTree", p, "allfunctions-100000",
+		// Script.PERFORMANCE);
+	}
 }
